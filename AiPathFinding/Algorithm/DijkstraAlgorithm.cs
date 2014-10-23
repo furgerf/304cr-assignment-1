420e5d58bdb4218aba3b05e1ff6d02ae4f1d5a02
mystyfly <mystyfly@gmail.com>
2014-10-23 18:35:04 +0100

    initial dijkstra implementation, seems to work...

--- AiPathFinding/Algorithm/DijkstraAlgorithm.cs
+++ AiPathFinding/Algorithm/DijkstraAlgorithm.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using AiPathFinding.Model;
 
 namespace AiPathFinding.Algorithm
@@ -32,29 +33,43 @@ namespace AiPathFinding.Algorithm
             PrepareData(from);
 
             ProcessNode(from);
+
+            while (_nodeDataMap[to].Item2 == int.MaxValue && _unvisitedNodes.Count > 0)
+                ProcessNode(_unvisitedNodes[0]);
         }
 
         private void ProcessNode(Node node)
         {
-            foreach (var e in node.Edges)
+            // update distance to all neighbors (if shorter)
+            foreach (var e in node.Edges.Where(ee => ee != null && ee.GetOtherNode(node).Cost != int.MaxValue))
             {
+                // get other node
                 var otherNode = e.GetOtherNode(node);
-                
-                //if (_nodeDataMap[otherNode].Item2 > _nodeDataMap[node].Item2 + otherNode.)
-                //_nodeDataMap[otherNode] = new Tuple<bool, int>(false, );
+
+                if (_nodeDataMap[otherNode].Item2 == int.MaxValue)
+                    _unvisitedNodes.Add(otherNode);
+
+                if (_nodeDataMap[otherNode].Item2 > _nodeDataMap[node].Item2 + otherNode.Cost)
+                    _nodeDataMap[otherNode] = new Tuple<bool, int>(false, _nodeDataMap[node].Item2 + otherNode.Cost);
             }
 
+            // mark current node as visited and move it to the other list
+            _nodeDataMap[node] = new Tuple<bool, int>(true, _nodeDataMap[node].Item2);
+            _visitedNodes.Add(node);
+            _unvisitedNodes.Remove(node);
         }
 
         private void PrepareData(Node startNode)
         {
             // add all nodes to the unvisited-node-list and tag nodes as unvisited with max cost
-            foreach (var nn in _graph.Nodes)
+            foreach (var nn in _graph.Nodes.Where(n => n != null))
             {
-                _unvisitedNodes.AddRange(nn);
-                foreach (var n in nn)
+                //_unvisitedNodes.AddRange(nn);
+                foreach (var n in nn.Where(n => n != null))
                     _nodeDataMap.Add(n, new Tuple<bool, int>(false, n == startNode ? 0 : int.MaxValue));
             }
+
+            _unvisitedNodes.Add(startNode);
         }
 
         #endregion
